de2f3d68a49837178ea1b63ad472f5ffc314aadb
khaya-zulu <kzulu321@gmail.com>
2022-09-07 15:48:19 +0200

    log resp

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -2,7 +2,7 @@ import { netlify } from "./netlify.mjs";
 
 console.log("Setting deploy preview environment variable...");
 
-await netlify(
+const test = await netlify(
   `/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env/DATABASE_URL?site=${process.env.NETLIFY_SITE_ID}`,
   {
     method: "PATCH",
@@ -12,5 +12,7 @@ await netlify(
     }),
   }
 );
+
+console.log({ test });
 
 console.log("Setting has been set.");
