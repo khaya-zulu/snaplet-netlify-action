2f21f76b00d37f8c867cf111b17b666e3c41ad39
khaya-zulu <kzulu321@gmail.com>
2022-09-07 15:52:10 +0200

    inclue site-id

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -3,7 +3,7 @@ import { netlify } from "./netlify.mjs";
 console.log("Setting deploy preview environment variable...");
 
 const test = await netlify(
-  `/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env/DATABASE_URL?site=${process.env.NETLIFY_SITE_ID}`,
+  `/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env/DATABASE_URL?site_id=${process.env.NETLIFY_SITE_ID}`,
   {
     method: "PATCH",
     body: JSON.stringify({
