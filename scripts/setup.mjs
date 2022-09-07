654c8d0a69859ca64d4501ebe6905b99c25b505c
khaya-zulu <kzulu321@gmail.com>
2022-09-07 15:13:25 +0200

    add netlify tokens

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -2,7 +2,7 @@ import { netlify } from "./netlify.mjs";
 
 console.log("Setting deploy preview environment variable...");
 
-console.log({ db_url: process.env.DATABASE_URL });
+console.log({ db_url: process.env.NETLIFY_ACCOUNT_ID });
 
 await netlify(
   `/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env/DATABASE_URL?site=${process.env.NETLIFY_SITE_ID}`,
