2c930804e636f44cb86fa56b5498d79bbb65e648
khaya-zulu <kzulu321@gmail.com>
2022-09-07 17:30:32 +0200

    update

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -2,15 +2,23 @@ import { netlify } from "./netlify.mjs";
 
 console.log("Setting deploy preview environment variable...");
 
-await netlify(
+const resp = await netlify(
   `/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env/DATABASE_URL?site_id=${process.env.NETLIFY_SITE_ID}`,
   {
     method: "PATCH",
     body: JSON.stringify({
       context: "branch-deploy",
-      value: process.env.DATABASE_URL,
+      value: [
+        {
+          context: "branch",
+          context_parameter: process.env.GITHUB_HEAD_REF,
+          value: process.env.DATABASE_URL,
+        },
+      ],
     }),
   }
 );
+
+console.log({ resp });
 
 console.log("Setting has been set.");
