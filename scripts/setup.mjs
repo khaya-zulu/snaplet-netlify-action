46255250be7e467a5f6382a415326753eaef6856
khaya-zulu <kzulu321@gmail.com>
2022-09-07 16:38:48 +0200

    update

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -2,29 +2,15 @@ import { netlify } from "./netlify.mjs";
 
 console.log("Setting deploy preview environment variable...");
 
-await Promise.all([
-  netlify(
-    `/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env/DATABASE_URL?site_id=${process.env.NETLIFY_SITE_ID}`,
-    {
-      method: "PATCH",
-      body: JSON.stringify({
-        context: "branch-deploy",
-        value: process.env.DATABASE_URL,
-      }),
-    }
-  ),
-  netlify(
-    `/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env/DATABASE_URL?site_id=${process.env.NETLIFY_SITE_ID}`,
-    {
-      method: "PATCH",
-      body: JSON.stringify({
-        context: "deploy-preview",
-        value: process.env.DATABASE_URL,
-      }),
-    }
-  ),
-]);
-
-console.log({ test });
+await netlify(
+  `/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env/DATABASE_URL?site_id=${process.env.NETLIFY_SITE_ID}`,
+  {
+    method: "PATCH",
+    body: JSON.stringify({
+      context: "branch-deploy",
+      value: process.env.DATABASE_URL,
+    }),
+  }
+);
 
 console.log("Setting has been set.");
