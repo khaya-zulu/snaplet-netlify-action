f50b12686cf1485e1ba1e8ce29948586143fc0c9
khaya-zulu <kzulu321@gmail.com>
2022-09-14 09:16:15 +0200

    commit

--- scripts/delete.mjs
+++ scripts/delete.mjs
@@ -1,35 +1,14 @@
 import { netlify } from "./netlify.mjs";
 
-/** Delete build hooks */
-const buildHooks = await netlify(
-  `/sites/${process.env.NETLIFY_SITE_ID}/build_hooks`,
-  { method: "GET" }
-);
-
-const buildHook = buildHooks.find(
-  (hook) => hook.branch === process.env.GITHUB_HEAD_REF
-);
-
-if (Boolean(buildHook)) {
-  console.log("Deleting build hook...");
-
-  await netlify(
-    `/sites/${process.env.NETLIFY_SITE_ID}/build_hooks/${buildHook.id}`,
-    { method: "DELETE" }
-  );
-
-  console.log("Build hook deleted.");
-}
-
 /** Delete env vars */
 const envVars = await netlify(
   `/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env?site=${process.env.NETLIFY_SITE_ID}`,
   { method: "GET" }
 );
 
-const envVar = envVars.find((item) => item.key === "DATABASE_URL")?.values;
+const envVar = envVars?.find((item) => item.key === "DATABASE_URL")?.values;
 
-const branchedEnvVar = envVar.find(
+const branchedEnvVar = envVar?.find(
   (item) =>
     item?.context_parameter === process.env.GITHUB_HEAD_REF &&
     item.context === "branch"
