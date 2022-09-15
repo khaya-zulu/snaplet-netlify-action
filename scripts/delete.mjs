c92b8d073e846e4f6e39645d1a2d15490a89c524
khaya-zulu <kzulu321@gmail.com>
2022-09-15 14:23:39 +0200

    commit

--- scripts/delete.mjs
+++ scripts/delete.mjs
@@ -28,4 +28,8 @@ if (Boolean(branchedEnvVar)) {
   );
 
   console.log(`${process.env.DATABASE_ENV_VAR} environment variable deleted.`);
+} else {
+  console.log(
+    `Could not found find ${process.env.DATABASE_ENV_VAR} linked to branch: ${process.env.GITHUB_HEAD_REF}`
+  );
 }
